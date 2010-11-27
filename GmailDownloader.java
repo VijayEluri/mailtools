6ee55e79620a8ef153e2f4b3e858ad511e0d5de3
Kartikaya Gupta <accounts.github@staktrace.com>
2010-11-26 23:12:31 -0500

    Add a -d option to delete messages as well. Yes, I know this code is butt-ugly but I'm in a hurry. Maybe I'll clean it up the next time I have to modify this code

--- GmailDownloader.java
+++ GmailDownloader.java
@@ -12,13 +12,21 @@ import javax.net.ssl.*;
 
 public class GmailDownloader {
     public static void main( String[] args ) throws Exception {
+        boolean delete = false;
         int start = 1;
         int end = -1;
         if (args.length > 0) {
+            int argix = 0;
+            if (args[ argix ].equals( "-d" )) {
+                delete = true;
+                argix++;
+            }
             try {
-                start = Integer.parseInt( args[0] );
-                if (args.length > 1) {
-                    end = Integer.parseInt( args[1] );
+                start = Integer.parseInt( args[ argix++ ] );
+                if (args.length > argix) {
+                    end = Integer.parseInt( args[ argix ] );
+                } else if (delete) {
+                    throw new NumberFormatException();
                 }
             } catch (NumberFormatException nfe) {
                 System.out.println( "Usage: java GmailDownloader [start [end]]" );
@@ -30,6 +38,13 @@ public class GmailDownloader {
                 System.out.println( "   the start value for where you left off. Optionally specify the" );
                 System.out.println( "   end value to limit the number of messages downloaded." );
                 System.out.println();
+                System.out.println( "      java GmailDownloader -d <start> <end>" );
+                System.out.println();
+                System.out.println( "   Deletes the messages in the given range. Usually you want to only" );
+                System.out.println( "   delete messages you have already downloaded. Note that once messages" );
+                System.out.println( "   are deleted, the messages after that are renumbered so that there are" );
+                System.out.println( "   no holes in the numbering. So start=1 end=1000 will delete the first" );
+                System.out.println( "   1000 messages, and message 1001 will get renumbered down to 1." );
                 return;
             }
         }
@@ -64,32 +79,45 @@ public class GmailDownloader {
             }
         }
 
-        System.out.println( "Downloading messages from [" + start + "] to [" + end + "]" );
-        for (int i = start; i <= end; i++) {
-            System.out.println( "Downloading message " + i );
-            pw.print( "ZZ FETCH " + i + " RFC822\r\n" ); pw.flush();
-            br.readLine();
-            String fn = i + ".msg";
-            while (fn.length() < 9) {
-                fn = "0" + fn;
+        if (delete) {
+            System.out.println( "Deleting messages from [" + start + "] to [" + end + "]" );
+            pw.print( "ZZ COPY " + start + ":" + end + " \"[Gmail]/Trash\"\r\n" ); pw.flush();
+            for (String s = br.readLine(); !s.startsWith( "ZZ OK" ); s = br.readLine()) {
+                if (! s.endsWith( " EXISTS" )) {
+                    continue;
+                }
+                StringTokenizer st = new StringTokenizer( s );
+                st.nextToken();
+                System.out.println( "Down to " + st.nextToken() + " messages" );
             }
-            PrintWriter file = new PrintWriter( new File( fn ) );
-            outer: while (true) {
-                String s = br.readLine();
-                while (s.endsWith( ")" )) {
-                    String t = br.readLine();
-                    if (t.startsWith( "ZZ OK" )) {
-                        if (s.length() > 1) {
-                            file.println( s.substring( 0, s.length() - 1 ) );
+        } else {
+            System.out.println( "Downloading messages from [" + start + "] to [" + end + "]" );
+            for (int i = start; i <= end; i++) {
+                System.out.println( "Downloading message " + i );
+                pw.print( "ZZ FETCH " + i + " RFC822\r\n" ); pw.flush();
+                br.readLine();
+                String fn = i + ".msg";
+                while (fn.length() < 9) {
+                    fn = "0" + fn;
+                }
+                PrintWriter file = new PrintWriter( new File( fn ) );
+                outer: while (true) {
+                    String s = br.readLine();
+                    while (s.endsWith( ")" )) {
+                        String t = br.readLine();
+                        if (t.startsWith( "ZZ OK" )) {
+                            if (s.length() > 1) {
+                                file.println( s.substring( 0, s.length() - 1 ) );
+                            }
+                            break outer;
                         }
-                        break outer;
+                        file.println( s );
+                        s = t;
                     }
                     file.println( s );
-                    s = t;
                 }
-                file.println( s );
+                file.close();
             }
-            file.close();
         }
 
         pw.print( "C LOGOUT\r\n" ); pw.flush();
